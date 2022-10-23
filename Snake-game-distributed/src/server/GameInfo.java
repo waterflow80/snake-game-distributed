e05a1ccd2f71186212782dbc51af85eb4c9306d0
Haroun <82417779+waterflow80@users.noreply.github.com>
2022-10-23 18:45:02 +0100

    Delete Snake-game-distributed directory

--- Snake-game-distributed/src/server/GameInfo.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class GameInfo {
-	public static List<Player> players = new ArrayList<>(); // list of all players currently playing
-}
